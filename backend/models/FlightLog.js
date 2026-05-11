608288386aeaaf4aa7556655f9be462fa6425d6e
Ariyan <ariy12005bst@gmail.com>
2026-05-10 20:30:43 -0400

    Add frontend admin authentication and CRUD improvements

--- backend/models/FlightLog.js
+++ backend/models/FlightLog.js
@@ -2,38 +2,17 @@ const mongoose = require("mongoose");
 
 const flightLogSchema = new mongoose.Schema(
   {
-    pilotName: {
+    pilot: {
       type: String,
       required: true,
     },
-
     aircraft: {
       type: String,
       required: true,
     },
-
-    departure: {
-      type: String,
-      required: true,
-    },
-
-    arrival: {
-      type: String,
-      required: true,
-    },
-
-    flightHours: {
-      type: Number,
-      required: true,
-    },
-
     date: {
       type: String,
-      required: true,
-    },
-
-    notes: {
-      type: String,
+      required: false,
     },
   },
   { timestamps: true }
